6f36045026cae4f906f9dbd9fd2a78c49faba913
Vitaly Gins <vitaly.gins@gmail.com>
2021-03-04 16:44:09 -0700

    lazy loading

--- js/populate-gallery.mjs
+++ js/populate-gallery.mjs
@@ -8,7 +8,7 @@ const populateGallery = () => {
     // insert the properties of each objects inside the array into the appropriate HTML field.
     gallery.innerHTML += `
        <figure>
-        <img src="https://picsum.photos/500/500?random=${i}" alt="">
+        <img src="https://picsum.photos/500/500?random=${i}" alt="random picsum image" loading="lazy">
       <figcaption></figcaption>
       </figure>
     `;
